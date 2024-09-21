5ba3d2d8cb3692c3ab1edd1727159ddfe00d00fd
vishnuvardhankorchella <vishnuvardhan6469@gmail.com>
2024-09-21 11:28:47 +0530

    Final file

---
+++ counter.js
@@ -0,0 +1,44 @@
+let counterElement;
+counterElement = document.getElementById("counterValue");
+
+function onIncrement() {
+    let previousCounterValue = counterElement.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) + 1;
+    counterElement.textContent = updatedCounterValue;
+
+    if (updatedCounterValue > 0) {
+        counterElement.style.color = "green";
+    } else if (updatedCounterValue < 0) {
+        counterElement.style.color = "red";
+    } else {
+        counterElement.style.color = "black";
+    }
+}
+
+function onDecrement() {
+    let previousCounterValue = counterElement.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) - 1;
+    counterElement.textContent = updatedCounterValue;
+
+    if (updatedCounterValue > 0) {
+        counterElement.style.color = "green";
+    } else if (updatedCounterValue < 0) {
+        counterElement.style.color = "red";
+    } else {
+        counterElement.style.color = "black";
+    }
+
+}
+
+function onReset() {
+    let updatedCounterValue = 0;
+    counterElement.textContent = updatedCounterValue;
+
+    if (updatedCounterValue > 0) {
+        counterElement.style.color = "green";
+    } else if (updatedCounterValue < 0) {
+        counterElement.style.color = "red";
+    } else {
+        counterElement.style.color = "black";
+    }
+}
